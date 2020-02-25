54898c281198a55611468f33930d2439be4b40e0
Mattia Pedone <mattiapedone93@gmail.com>
2020-02-25 19:45:54 +0100

    up

--- index.php
+++ index.php
@@ -7,10 +7,18 @@
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
+    <!-- link al nostro stile -->
+    <link rel="stylesheet" href="dist/app.css">
+    <!-- link google font -->
+    <link href="https://fonts.googleapis.com/css?family=Lato&display=swap" rel="stylesheet">
     <title></title>
   </head>
   <body>
-    <header></header>
+    <header>
+      <div class="container">
+        <img src="img/logo.png" alt="logo" />
+      </div>
+    </header>
 
     <main>
       <div class="wrapper_85">
@@ -20,6 +28,7 @@
             <h2><?php echo $data['title']; ?></h2>
             <h3><?php echo $data['author']; ?></h3>
             <h4><?php echo $data['year']; ?></h4>
+          
           </div>
         <?php }  ?>
 
