b29c8f188aed71a54947e5c40aa547cf9607b5b1
Mattia Pedone <mattiapedone93@gmail.com>
2020-02-26 11:57:16 +0100

    aggiunto secondo file index

---
+++ index2.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <!-- link al nostro stile -->
+    <link rel="stylesheet" href="dist/app.css">
+    <title>Spotify Web</title>
+  </head>
+  <body>
+    <header>
+      <div class="container">
+        <img src="img/logo.png" alt="logo" />
+      </div>
+    </header>
+
+    <main>
+      <div class="cds-container container">
+        
+        
+      </div>
+    </main>
+    <script id="entry-template" type="text/x-handlebars-template">
+      <div class="album">
+        <img src= "{{poster}}" alt="">
+        <h3>{{title}}</h3>
+        <span class="author">{{author}}</span>
+        <span class="year">{{year}}</span>
+      </div>
+    </script>
+    <script src="dist/app.js"></script>
+  </body>
+</html>
