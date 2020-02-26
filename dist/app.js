f6d65b84850001d42c9a2ab603961f8a15e86bc2
Mattia Pedone <mattiapedone93@gmail.com>
2020-02-26 14:59:18 +0100

    up

--- dist/app.js
+++ dist/app.js
@@ -15822,9 +15822,20 @@ $(document).ready(function () {
     url: 'http://localhost/php-ajax-dischi/ajax-database.php',
     method: 'GET',
     success: function success(data) {
+      var source = $('#entry-template').html();
+      var template = Handlebars.compile(source);
+
       for (var i = 0; i < data.length; i++) {
         var thisCD = data[i];
         console.log(thisCD);
+        var context = {
+          poster: thisCD.poster,
+          title: thisCD.title,
+          author: thisCD.author,
+          year: thisCD.year
+        };
+        var html = template(context);
+        $('.cds-container').append(html);
       }
     },
     error: function error(_error) {
--- src/app.js
+++ src/app.js
@@ -9,11 +9,19 @@ $(document).ready(function() {
       url: 'http://localhost/php-ajax-dischi/ajax-database.php',
       method: 'GET',
       success: function (data) {
+        var source = $('#entry-template').html();
+        var template = Handlebars.compile(source);
         for (var i = 0; i < data.length; i++) {
           var thisCD = data[i];
           console.log(thisCD);
-          
-          
+          var context = { 
+            poster: thisCD.poster,
+            title: thisCD.title, 
+            author: thisCD.author,
+            year: thisCD.year 
+          };
+          var html = template(context);
+          $('.cds-container').append(html);
         }
         
       },
