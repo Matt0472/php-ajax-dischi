733a7684e8d4b44ce748b6cb8f710209d6813f6e
Mattia Pedone <mattiapedone93@gmail.com>
2020-02-26 15:12:14 +0100

    up: trasformato un altro pezzo in function

--- src/app.js
+++ src/app.js
@@ -9,25 +9,7 @@ $(document).ready(function() {
       url: 'http://localhost/php-ajax-dischi/ajax-database.php',
       method: 'GET',
       success: function (data) {
-        var source = $('#entry-template').html();
-        var template = Handlebars.compile(source);
-        for (var i = 0; i < data.length; i++) {
-          if (data.length > 0) {
-            var thisCD = data[i];
-            console.log(thisCD);
-            var context = {
-              poster: thisCD.poster,
-              title: thisCD.title,
-              author: thisCD.author,
-              year: thisCD.year
-            };
-            var html = template(context);
-            $('.cds-container').append(html);
-          } else {
-            printNoResult();
-          }
-        }
-        
+        printResult(data);
       },
       error: function(error) {
         alert('errore');
@@ -38,6 +20,28 @@ $(document).ready(function() {
 
 
 // FUNCTION
+
+function printResult(result) {
+  var source = $('#entry-template').html();
+  var template = Handlebars.compile(source);
+  for (var i = 0; i < result.length; i++) {
+    if (result.length > 0) {
+      var thisCD = result[i];
+      console.log(thisCD);
+      var context = {
+        poster: thisCD.poster,
+        title: thisCD.title,
+        author: thisCD.author,
+        year: thisCD.year
+      };
+      var html = template(context);
+      $('.cds-container').append(html);
+    } else {
+      printNoResult();
+    }
+  }
+}
+
 
 function printNoResult() {
   var source = $('#noresult-template').html();
